12b79c3ca0d276c75eed40f4b9a558e38a5c08e8
Vasyl Kichula <104771020+vkich7@users.noreply.github.com>
2024-02-27 08:36:59 +0200

    Add files via upload

---
+++ Class/ClassTask.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Class
+{
+    //TODO: Create class Rectangle here
+
+    //TODO: Define two private double fields: sideA, sideB
+    //TODO: Define constructor with two double parameters: a, b. Assign parameters to sides
+    //TODO: Define constructor with double parameter: a. Parameter sets side A of rectangle, and B-side always equals 5
+    //TODO: Define constructor without parameters. A-side of rectangle equals 4, B-side - 3
+    //TODO: Define method that returns value of A-side: GetSideA
+    //TODO: Define method that returns value of B-side: GetSideB
+    //TODO: Define method that calculates and returns value of area: Area
+    //TODO: Define method that calculates and returns value of perimeter: Perimeter
+    //TODO: Define method that checks if rectangle is a square: IsSquare. Returns true if it is a square, false if it's not.
+    //TODO: Define method that swaps values of rectangle sides: ReplaceSides
+
+
+
+    //TODO: Create class ArrayRectangles here
+
+    //TODO: Define private field that is array of rectangles: rectangle_array
+    //TODO: Define constructor with gets parameter: n. Constructor should create an empty array of rectangles with length of n
+    //TODO: Define constructor that gets enumerable or array of rectangles. Constructor should assign them to its field
+    //TODO: Define method that adds rectangle on the first empty place of array field: AddRectangle. Returns true if array has empty space, if not
+    //TODO: Define method that returns number of rectangle with max value of area: NumberMaxArea. Numbering starts from 0
+    //TODO: Define method that returns number of rectangle with min value of perimeter: NumberMinPerimeter. Numbering starts from 0
+    //TODO: Define method that returns amount of squares in array of rectangles: NumberSquare
+
+}
